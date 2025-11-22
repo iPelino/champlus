d391f90df9be347a5894f5b83105e119592534a7
iPelino <pelmut2000@gmail.com>
2025-11-22 06:15:08 +0200

    feat: Frontend application with TypeScript (Closes #28, #39, #40)

---
+++ frontend/src/components/TestButton.tsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+
+export default function TestButton() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
